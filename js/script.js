349e56eda1f5e7b63cc05018149e839f71391ea3
Eric Negreido <ericnegreidooo@gmail.com>
2023-06-02 17:21:40 -0300

    agregar elemento al dom y funcionamiento del boton

--- js/script.js
+++ js/script.js
@@ -16,4 +16,54 @@ class item {    // El producto en si que se agrega a la lista con su costo corre
     }
 }
 
+//REGISTROS
 const register = [];
+
+const person = [];
+
+//FUNCIONES
+let aux;
+function addRegister(){
+    let newR = document.getElementById("newAdd");
+    newR.style.display = "block";
+
+
+}
+
+function acceptRegister(){
+    //Cargar valores
+    let aux1 = document.getElementById("name").value;
+    let aux2 = document.getElementById("subjet").value;
+    let aux3 = document.getElementById("product").value;
+    let aux4 = document.getElementById("cost").value;
+
+    // Ocultar el formulario
+    let newR = document.getElementById("newAdd");
+    newR.style.display = "none";
+
+    // Limpiar los campos del formulario
+    document.getElementById("name").value = "";
+    document.getElementById("subjet").value = "";
+    document.getElementById("product").value = "";
+    document.getElementById("cost").value = "";
+
+    //Agregar al DOM
+    const newRow = document.createElement("div");
+    newRow.className = "row";
+    newRow.textContent = `${aux1} ${aux2} ${aux3} ${aux4}  `;
+    let reg = document.getElementById("register");
+    reg.append(newRow);
+
+}
+
+// // let newperso = document.getElementById("new");
+// // newperso.onclick = () => {new personal("eric","cajero")};
+
+let add = document.getElementById("add");
+add.onclick = addRegister; 
+
+let acepted = document.getElementById("creat");
+acepted.onclick = acceptRegister;
+
+
+
