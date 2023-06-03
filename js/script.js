b2f42322dfce6303c26bc658549aa255afb0c2f8
Eric Negreido <ericnegreidooo@gmail.com>
2023-06-03 14:08:10 -0300

    boton nuevo y correciones

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 //OBJETOS
 
-class personal {        //Nombre de la peronsa, y puesto de trabajo, con su lista personal de deudas y pagos Ej = personal(roberto, mozo)
-    constructor (name, job){
+class personal { //Nombre de la peronsa, y puesto de trabajo, con su lista personal de deudas y pagos Ej = personal(roberto, mozo)
+    constructor(name, job) {
         this.name = name;
         this.job = job;
         let peronal_list = [];
     }
 }
 
-class item {    // El producto en si que se agrega a la lista con su costo correspondiente , Ej = item(adelanto, plata, 1000) o item(fiar, coca350, 250)
-    constructor (subject, product, cost){
+class item { // El producto en si que se agrega a la lista con su costo correspondiente , Ej = item(adelanto, plata, 1000) o item(fiar, coca350, 250)
+    constructor(subject, product, cost) {
         this.subject = subject;
         this.product = product;
-        this.cost = cost;      
+        this.cost = cost;
     }
 }
 
@@ -23,47 +23,81 @@ const person = [];
 
 //FUNCIONES
 let aux;
-function addRegister(){
-    let newR = document.getElementById("newAdd");
-    newR.style.display = "block";
 
-
+function show(num) {
+    if(0 == num){
+        let newR = document.getElementById("newadd");
+        newR.style.display = "block";
+    }else if(1 == num){
+        let newE = document.getElementById("newemployee");
+        newE.style.display = "block";
+    }
+    
 }
 
-function acceptRegister(){
+function acceptRegister() {
     //Cargar valores
     let aux1 = document.getElementById("name").value;
     let aux2 = document.getElementById("subjet").value;
     let aux3 = document.getElementById("product").value;
     let aux4 = document.getElementById("cost").value;
+    let aux5 = document.getElementById("pay").value;
 
     // Ocultar el formulario
-    let newR = document.getElementById("newAdd");
+    let newR = document.getElementById("newadd");
     newR.style.display = "none";
+
+    if( aux1 != "" && aux2 != "" && aux3 != "" && aux4 != ""){ // Condicional para no crear estrucura vacia
+        
+        //Agregar al DOM
+        const newRow = document.createElement("div");
+        newRow.className = "row";
+        newRow.textContent = `${aux1}  ${aux2}  ${aux3}  ${aux4}  ${aux5}`;
+        let reg = document.getElementById("register");
+        reg.append(newRow);
+    }
 
     // Limpiar los campos del formulario
     document.getElementById("name").value = "";
     document.getElementById("subjet").value = "";
     document.getElementById("product").value = "";
     document.getElementById("cost").value = "";
-
-    //Agregar al DOM
-    const newRow = document.createElement("div");
-    newRow.className = "row";
-    newRow.textContent = `${aux1} ${aux2} ${aux3} ${aux4}  `;
-    let reg = document.getElementById("register");
-    reg.append(newRow);
+    document.getElementById("pay").value = "";
 
 }
 
-// // let newperso = document.getElementById("new");
-// // newperso.onclick = () => {new personal("eric","cajero")};
+function acceptEmployee(){
+    //Cargar valores
+    let aux1 = document.getElementById("newname").value;
+    let aux2 = document.getElementById("job").value;
+
+     // Ocultar el formulario
+    let newE = document.getElementById("newemployee");
+    newE.style.display = "none";
+    if( aux1 != "" && aux2 != "" ){ // Condicional para no crear estrucura vacia
+        
+        //Agregar al DOM
+        const newOption = document.createElement("option");
+        newOption.textContent = `${aux1}`;
+        let reg = document.getElementById("name");
+        reg.appendChild(newOption);
+    }
+
+    document.getElementById("newname").value= "";
+    document.getElementById("job").value = "";
+}
+
+//Eventos
 
 let add = document.getElementById("add");
-add.onclick = addRegister; 
+add.onclick = () => {show(0)};
 
-let acepted = document.getElementById("creat");
-acepted.onclick = acceptRegister;
+let accept = document.getElementById("creat");
+accept.onclick = acceptRegister;
 
+let newPerso = document.getElementById("new");
+newPerso.onclick = () => {show(1)};
 
+let creatEmployee = document.getElementById("accept");
+creatEmployee.onclick = acceptEmployee;
 
